987000e773ee76f79739206dd536c61fc03b4632
aditya vegulla <you@example.com>
2022-01-31 22:24:16 +0530

    modified

--- Sample_question.cpp
+++ Sample_question.cpp
@@ -3,15 +3,21 @@ using namespace std;
 
 int main(){
 
-    int i,sum;
+    int i,sum=0,n;
     cout<<"Enter the value of n:"<<endl;
     cin>>i;
+    //O(n)
     if(i>0){
         for(int j=1;j<=i;j++){
-            sum += i;
+            sum += j;
+
         }
     }
-    cout << "Sum of natural num is:"<< sum <<endl;
-
+    cout << "Sum of nummbers is: "<< sum <<endl;
+    //O(1)
+    if(i>0){
+        n = (i*(i+1))/2;
+        cout <<"sum of natural numbers is: "<<n<<endl;
+    }
     return 0;
 }
